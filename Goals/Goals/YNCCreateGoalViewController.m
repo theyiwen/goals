a62797c793b6e62864e4fc0f6b33df4805565a8f
Calvin Lee <calvers@gmail.com>
2015-09-20 11:48:41 -0700

    less ugly

--- Goals/Goals/YNCCreateGoalViewController.m
+++ Goals/Goals/YNCCreateGoalViewController.m
@@ -16,14 +16,12 @@
 
 @property (strong, nonatomic) UIView *container;
 
-@property (strong, nonatomic) UILabel *goalTitleLabel;
 @property (strong, nonatomic) UILabel *goalDescriptionLabel;
 @property (strong, nonatomic) UILabel *goalTypeLabel;
 @property (strong, nonatomic) UILabel *goalMembersLabel;
-@property (strong, nonatomic) UILabel *goalDurationLabel;
 
 @property (strong, nonatomic) UITextField *goalTitleTextField;
-@property (strong, nonatomic) UITextField *goalDescriptionTextField;
+@property (strong, nonatomic) UITextView *goalDescriptionTextView;
 @property (strong, nonatomic) NSMutableArray *typeButtons;
 @property (strong, nonatomic) UIButton *dailyTypeButton;
 @property (strong, nonatomic) UIButton *sumTypeButton;
@@ -41,19 +39,22 @@
   self.view.backgroundColor = [UIColor whiteColor];
   UIView *container = self.container = [[UIView alloc] init];
   
-  UILabel *goalTitleLabel = self.goalTitleLabel = [[UILabel alloc] init];
-  goalTitleLabel.text = @"GOAL TITLE:";
   UILabel *goalDescriptionLabel = self.goalDescriptionLabel = [[UILabel alloc] init];
   goalDescriptionLabel.text = @"DESCRIPTION:";
   UILabel *goalTypeLabel = self.goalTypeLabel = [[UILabel alloc] init];
   goalTypeLabel.text = @"TYPE:";
   UILabel *goalMembersLabel = self.goalMembersLabel = [[UILabel alloc] init];
   goalMembersLabel.text = @"WITH:";
-  UILabel *goalDurationLabel = self.goalDurationLabel = [[UILabel alloc] init];
-  goalDurationLabel.text = @"DURATION:";
   
   UITextField *goalTitleTextField = self.goalTitleTextField = [[UITextField alloc] init];
-  UITextField *goalDescriptionTextField = self.goalDescriptionTextField = [[UITextField alloc] init];
+  goalTitleTextField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"GOAL" attributes:@{NSForegroundColorAttributeName: [UIColor blackColor]}];
+  [goalTitleTextField setBorderStyle:UITextBorderStyleRoundedRect];
+   
+  UITextView *goalDescriptionTextView = self.goalDescriptionTextView = [[UITextView alloc] init];
+  goalDescriptionTextView.layer.borderWidth = 1.0f;
+  goalDescriptionTextView.layer.borderColor = [[UIColor grayColor] CGColor];
+  goalDescriptionTextView.layer.cornerRadius = 8;
+  
   UIButton *dailyTypeButton = self.dailyTypeButton = [[UIButton alloc] init];
   UIButton *sumTypeButton = self.sumTypeButton = [[UIButton alloc] init];
   NSMutableArray *typeButtons = self.typeButtons = [[NSMutableArray alloc] initWithArray:@[dailyTypeButton, sumTypeButton]];
@@ -65,23 +66,22 @@
   [sumTypeButton setTitle:@"TOTAL" forState:UIControlStateNormal];
   sumTypeButton.tag = sum;
   [sumTypeButton addTarget:self action:@selector(selectType:) forControlEvents:UIControlEventTouchUpInside];
+  
   UITextField *goalDurationTextField = self.goalDurationTextField = [[UITextField alloc] init];
-  goalTitleTextField.backgroundColor = [UIColor grayColor];
-  goalDurationTextField.backgroundColor = [UIColor grayColor];
-  goalDescriptionTextField.backgroundColor = [UIColor grayColor];
+  goalDurationTextField.keyboardType = UIKeyboardTypeNumberPad;
+  goalDurationTextField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"DURATION" attributes:@{NSForegroundColorAttributeName: [UIColor blackColor]}];
+  [goalDurationTextField setBorderStyle:UITextBorderStyleRoundedRect];
   
   UIButton *submitButton = self.submitButton = [[UIButton alloc] init];
   [submitButton setTitle:@"SUBMIT" forState:UIControlStateNormal];
   [submitButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
   [submitButton addTarget:self action:@selector(submitButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
   
-  goalTitleLabel.font = [YNCFont standardFont];
   goalDescriptionLabel.font = [YNCFont standardFont];
   goalTypeLabel.font = [YNCFont standardFont];
   goalMembersLabel.font = [YNCFont standardFont];
-  goalDurationLabel.font = [YNCFont standardFont];
   goalTitleTextField.font = [YNCFont standardFont];
-  goalDescriptionTextField.font = [YNCFont standardFont];
+  goalDescriptionTextView.font = [YNCFont standardFont];
   goalDurationTextField.font = [YNCFont standardFont];
   goalDurationTextField.font = [YNCFont standardFont];
   dailyTypeButton.titleLabel.font = [YNCFont standardFont];
@@ -89,30 +89,26 @@
   submitButton.titleLabel.font = [YNCFont standardFont];
   
   [self.view addSubview:container];
-  [container addSubview:goalTitleLabel];
   [container addSubview:goalTitleTextField];
   [container addSubview:goalTypeLabel];
   [container addSubview:dailyTypeButton];
   [container addSubview:sumTypeButton];
-  [container addSubview:goalDurationLabel];
   [container addSubview:goalDurationTextField];
   [container addSubview:goalMembersLabel];
   [container addSubview:goalDescriptionLabel];
-  [container addSubview:goalDescriptionTextField];
+  [container addSubview:goalDescriptionTextView];
   [container addSubview:submitButton];
   
-  NSDictionary *views = NSDictionaryOfVariableBindings(container, goalTitleLabel, goalTitleTextField, goalTypeLabel, dailyTypeButton, sumTypeButton, goalDurationLabel, goalDurationTextField, goalMembersLabel, goalDescriptionLabel, goalDescriptionTextField, submitButton);
+  NSDictionary *views = NSDictionaryOfVariableBindings(container, goalTitleTextField, goalTypeLabel, dailyTypeButton, sumTypeButton, goalDurationTextField, goalMembersLabel, goalDescriptionLabel, goalDescriptionTextView, submitButton);
   YNCAutoLayout *autoLayout = [[YNCAutoLayout alloc] initWithViews:views];
-  [autoLayout addVflConstraint:@"V:|-100-[goalTitleLabel]-20-[goalTypeLabel]-20-[goalDurationLabel]-20-[goalMembersLabel]-20-[goalDescriptionLabel]-100-[submitButton]" toView:container];
-  [autoLayout addVflConstraint:@"V:|-100-[goalTitleTextField(25)]" toView:container];
-  [autoLayout addVflConstraint:@"V:|-134-[dailyTypeButton]" toView:container];
-  [autoLayout addVflConstraint:@"V:|-134-[sumTypeButton]" toView:container];
-  [autoLayout addVflConstraint:@"V:|-175-[goalDurationTextField(25)]" toView:container];
-  [autoLayout addVflConstraint:@"V:|-250-[goalDescriptionTextField(100)]" toView:container];
-  [autoLayout addVflConstraint:@"H:|[goalTitleLabel]-10-[goalTitleTextField(200)]" toView:container];
+  [autoLayout addVflConstraint:@"V:|-100-[goalTitleTextField(25)]-20-[goalTypeLabel]-20-[goalDurationTextField(25)]-20-[goalMembersLabel]-20-[goalDescriptionLabel]-100-[submitButton]" toView:container];
+  [autoLayout addVflConstraint:@"V:|-139-[dailyTypeButton]" toView:container];
+  [autoLayout addVflConstraint:@"V:|-139-[sumTypeButton]" toView:container];
+  [autoLayout addVflConstraint:@"V:|-280-[goalDescriptionTextView(100)]" toView:container];
+  [autoLayout addVflConstraint:@"H:|[goalTitleTextField(200)]" toView:container];
   [autoLayout addVflConstraint:@"H:|[goalTypeLabel]-10-[dailyTypeButton]-10-[sumTypeButton]" toView:container];
-  [autoLayout addVflConstraint:@"H:|[goalDurationLabel]-10-[goalDurationTextField(200)]" toView:container];
-  [autoLayout addVflConstraint:@"H:|[goalDescriptionLabel]-10-[goalDescriptionTextField(200)]" toView:container];
+  [autoLayout addVflConstraint:@"H:|[goalDurationTextField(200)]" toView:container];
+  [autoLayout addVflConstraint:@"H:|[goalDescriptionLabel]-10-[goalDescriptionTextView(200)]" toView:container];
   [autoLayout addVflConstraint:@"H:[submitButton]-50-|" toView:container];
   [autoLayout addVflConstraint:@"V:|[container]|" toView:self.view];
   [autoLayout addVflConstraint:@"H:|[container]|" toView:self.view];
@@ -148,9 +144,9 @@
    */
   NSArray *usersList = [usersBuilder copy];
   
-  [YNCGoal createAndSaveGoalWithTitle:self.goalTitleTextField.text desc:self.goalDescriptionTextField.text type:self.goalType duration:duration usersList:usersList];
+  [YNCGoal createAndSaveGoalWithTitle:self.goalTitleTextField.text desc:self.goalDescriptionTextView.text type:self.goalType duration:duration usersList:usersList];
   [self.navigationController popViewControllerAnimated:YES];
-  // TODO (REFRESH list view after)
+  // TODO (REFRESH list view with callbacks)
 }
 
 @end
