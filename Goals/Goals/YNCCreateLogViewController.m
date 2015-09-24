d1d8947a6177083da154766799fe65ec4cc19eff
Yiwen Zhan <theyiwen@gmail.com>
2015-09-23 20:39:33 -0700

    slightly better log ui

--- Goals/Goals/YNCCreateLogViewController.m
+++ Goals/Goals/YNCCreateLogViewController.m
@@ -10,16 +10,19 @@
 #import "YNCAutoLayout.h"
 #import "YNCLog.h"
 #import "YNCFont.h"
+#import "YNCColor.h"
 
 @interface YNCCreateLogViewController ()<UITextFieldDelegate, UITextViewDelegate>
 
 @property (strong, nonatomic) UITextField *countEntry;
-@property (strong, nonatomic) UITextView *notesEntry;
+@property (strong, nonatomic) UITextField *notesEntry;
 @property (strong, nonatomic) UILabel *countPlaceholder;
 @property (strong, nonatomic) UILabel *notesPlaceholder;
 @property (strong, nonatomic) UIButton *submitButton;
 @property (strong, nonatomic) UIButton *cancelButton;
 @property (strong, nonatomic) UIView *container;
+@property (strong, nonatomic) UIView *countBottomBorder;
+@property (strong, nonatomic) UIView *notesBottomBorder;
 
 @end
 
@@ -28,52 +31,67 @@
 - (void)loadView {
   self.view = [[UIView alloc] init];
   UITextField *countEntry = self.countEntry = [[UITextField alloc] init];
-  UITextView *notesEntry = self.notesEntry = [[UITextView alloc] init];
+  UITextField *notesEntry = self.notesEntry = [[UITextField alloc] init];
   UILabel *countPlaceholder = self.countPlaceholder = [[UILabel alloc] init];
   UILabel *notesPlaceholder = self.notesPlaceholder = [[UILabel alloc] init];
   UIButton *submitButton = self.submitButton = [[UIButton alloc] init];
   UIButton *cancelButton = self.cancelButton = [[UIButton alloc] init];
   UIView *container = self.container = [[UIView alloc] init];
+  UIView *countBottomBorder = self.countBottomBorder = [[UIView alloc] init];
+  UIView *notesBottomBorder = self.notesBottomBorder = [[UIView alloc] init];
+
 
   [self.view addSubview:container];
   [container addSubview:countEntry];
   [container addSubview:notesEntry];
   [container addSubview:countPlaceholder];
   [container addSubview:notesPlaceholder];
+  [container addSubview:countBottomBorder];
+  [container addSubview:notesBottomBorder];
   [container addSubview:submitButton];
   [container addSubview:cancelButton];
   
   NSDictionary *views = NSDictionaryOfVariableBindings(container, countEntry, notesEntry,
                                                        countPlaceholder, notesPlaceholder,
-                                                       submitButton, cancelButton);
+                                                       submitButton, cancelButton, countBottomBorder, notesBottomBorder);
   YNCAutoLayout *autoLayout = [[YNCAutoLayout alloc] initWithViews:views];
-  [autoLayout addConstraintForView:container withSize:CGSizeMake(300, 300) toView:container];
-  [autoLayout addVflConstraint:@"V:|-10-[cancelButton]-20-[countEntry]-20-[notesEntry(50)]-20-[submitButton]" toView:container];
+//  [autoLayout addConstraintForView:container withSize:CGSizeMake(300, 300) toView:container];
+  [autoLayout addVflConstraint:@"V:|-10-[cancelButton]-20-[countEntry]-5-[countBottomBorder(2)]-20-[notesEntry]-5-[notesBottomBorder(2)]-20-[submitButton]-20-|" toView:container];
   [autoLayout addVflConstraint:@"V:|-100-[container]" toView:self.view];
+  [autoLayout addVflConstraint:@"H:|-50-[container]-50-|" toView:self.view];
+
   [autoLayout addVflConstraint:@"H:|-10-[cancelButton]" toView:container];
   [autoLayout addVflConstraint:@"H:|-20-[countEntry]-20-|" toView:container];
   [autoLayout addVflConstraint:@"H:|-20-[notesEntry]-20-|" toView:container];
-  [autoLayout addConstraintForViews:@[countEntry, notesEntry, submitButton, container, self.view]
+  [autoLayout addConstraintForViews:@[submitButton, container, self.view]
                 equivalentAttribute:NSLayoutAttributeCenterX
                              toView:self.view];
-  [autoLayout addCenteringConstraintForViews:@[countEntry, countPlaceholder]
-                                      toView:container];
-  [autoLayout addCenteringConstraintForViews:@[notesEntry, notesPlaceholder]
-                                      toView:container];
+  [autoLayout addConstraintForViews:@[countEntry, countPlaceholder, countBottomBorder] equivalentAttribute:NSLayoutAttributeLeft toView:self.view];
+  [autoLayout addConstraintForViews:@[countEntry, countPlaceholder] equivalentAttribute:NSLayoutAttributeTop toView:self.view];
+  [autoLayout addConstraintForViews:@[notesEntry, notesPlaceholder, notesBottomBorder] equivalentAttribute:NSLayoutAttributeLeft toView:self.view];
+  [autoLayout addConstraintForViews:@[notesEntry, notesPlaceholder] equivalentAttribute:NSLayoutAttributeTop toView:self.view];
+  [autoLayout addConstraintForViews:@[notesEntry, notesBottomBorder, countEntry, countBottomBorder] equivalentAttribute:NSLayoutAttributeWidth toView:self.view];
   [autoLayout addConstraintForView:submitButton withSize:CGSizeMake(75,75) toView:submitButton];
   [self.cancelButton setTitle:@"X" forState:UIControlStateNormal];
   [self.cancelButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
   self.cancelButton.titleLabel.font = [YNCFont standardFont];
   self.submitButton.enabled = NO;
-  self.countPlaceholder.text = @"add number";
-  self.notesPlaceholder.text = @"add notes";
+  self.countPlaceholder.text = @"add number".uppercaseString;
+  self.notesPlaceholder.text = @"add notes".uppercaseString;
   self.countEntry.delegate = self;
   self.notesEntry.delegate = self;
-  self.countEntry.font = [YNCFont standardFont];
-  self.notesPlaceholder.font = [YNCFont standardFont];
-  self.countPlaceholder.font = [YNCFont standardFont];
-  self.notesEntry.font = [YNCFont standardFont];
+  self.countEntry.font = [UIFont fontWithName:[YNCFont semiBoldFontName] size:20];
+  self.notesPlaceholder.font = [UIFont fontWithName:[YNCFont semiBoldFontName] size:20];
+  self.countPlaceholder.font = [UIFont fontWithName:[YNCFont semiBoldFontName] size:20];
+  self.notesEntry.font = [UIFont fontWithName:[YNCFont semiBoldFontName] size:20];
   self.notesEntry.text = @"";
+  countBottomBorder.backgroundColor = [YNCColor tealColor];
+  notesBottomBorder.backgroundColor = [YNCColor tealColor];
+  self.notesEntry.tag = 2;
+  self.countEntry.tag = 1;
+  [self.countEntry addTarget:self
+                      action:@selector(textFieldDidChange:)
+            forControlEvents:UIControlEventEditingChanged];
 
   self.container.backgroundColor = [UIColor whiteColor];
   
@@ -87,28 +105,31 @@
 }
 
 - (void)textFieldDidBeginEditing:(UITextField *)textField {
-  if (!self.countPlaceholder.hidden) {
+  if (textField.tag == 1) {
     self.countPlaceholder.hidden = YES;
   }
-}
-
-- (void)textViewDidBeginEditing:(UITextView *)textView {
-  if (!self.notesPlaceholder.hidden) {
+  if (textField.tag == 2) {
     self.notesPlaceholder.hidden = YES;
   }
 }
 
-- (void)textFieldDidEndEditing:(UITextField *)textField {
-  if (self.countEntry.text.length > 0) {
+- (void)textFieldDidChange:(UITextField *)textField {
+  if (textField.tag == 1 && self.countEntry.text.length > 0) {
     self.submitButton.enabled = YES;
-  } else {
-    self.countPlaceholder.hidden = NO;
   }
 }
 
-- (void)textViewDidEndEditing:(UITextView *)textView {
-  if (self.notesEntry.text.length == 0) {
-    self.notesPlaceholder.hidden = NO;
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+  if (textField.tag == 1) {
+    if (self.countEntry.text.length > 0) {
+      self.submitButton.enabled = YES;
+    } else {
+      self.countPlaceholder.hidden = NO;
+    }
+  } else if (textField.tag == 2) {
+    if (self.notesEntry.text.length == 0) {
+      self.notesPlaceholder.hidden = NO;
+    }
   }
 }
 
